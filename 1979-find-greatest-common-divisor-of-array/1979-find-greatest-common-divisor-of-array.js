d80934dc26e6a65706665174d5638c16e824b340
Abhineet <abhineet.am13@gmail.com>
2026-07-18 21:16:36 +0530

    Time: 0 ms (100%), Space: 53.6 MB (83.7%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+
+    while (max !== 0) {
+        let temp = max;
+        max = min % max;
+        min = temp;
+    }
+    return min;
+};
