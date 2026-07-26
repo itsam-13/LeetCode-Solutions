ac0a9e25fcdc23bc907016c01502130768ac7cb6
Abhineet <abhineet.am13@gmail.com>
2026-07-26 21:07:52 +0530

    Time: 5 ms (87.05%), Space: 59.8 MB (27.23%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function (nums) {
+
+    let max1 = -Infinity, max3 = -Infinity, max2 = -Infinity;
+    let min1 = Infinity, min2 = Infinity;
+
+    for (let num of nums) {
+
+        if (num > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max3 = max2;
+            max2 = num;
+        } else if (num > max3) max3 = num;
+
+        if (num < min1) {
+            min2 = min1;
+            min1 = num;
+        } else if (num < min2) min2 = num;
+        
+    }
+    return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+};
