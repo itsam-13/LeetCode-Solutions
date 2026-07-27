abfef902d26252727d66762d9aca11d08b8010ab
Abhineet <abhineet.am13@gmail.com>
2026-07-27 20:09:55 +0530

    Time: 1 ms (77.68%), Space: 56.3 MB (13.73%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+
+    let max1 = -Infinity, max2 = -Infinity;
+    for (let num of nums) {
+
+        if (num > max1) {
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max2 = num;
+        }
+    }
+    return (max1 - 1) * (max2 - 1);
+};
