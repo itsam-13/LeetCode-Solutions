8d8160c98555ef6f02d80cbb9abe7cf87cf5a746
Abhineet <abhineet.am13@gmail.com>
2026-08-11 21:28:32 +0530

    Time: 1 ms (45.65%), Space: 55.7 MB (41.3%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function(nums) {
+    const set = new Set(nums);
+
+    let sum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1] + 1) {
+            sum += nums[i];
+        } else {
+            break;
+        }
+    }
+
+    while (set.has(sum)) {
+        sum++;
+    }
+
+    return sum;
+};
